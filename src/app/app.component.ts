45ad3326701d4de802de1d70bd8cdc8cac068810
SjorsJessen <sjors.jessen@gmail.com>
2023-01-13 22:43:12 +0100

    Setup of basic Ng3 project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { NgtAmbientLight, NgtSpotLight, NgtPointLight } from "@angular-three/core/lights";
+import { NgtCanvas } from "@angular-three/core";
+import { CubesComponent } from "./cubes/cubes.component";
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: true,
+  templateUrl: 'app.component.html',
+  imports: [NgtCanvas, NgtAmbientLight, NgtSpotLight, NgtPointLight, CubesComponent]
 })
-export class AppComponent {
-  title = 'ng3';
-}
+export class AppComponent {}
