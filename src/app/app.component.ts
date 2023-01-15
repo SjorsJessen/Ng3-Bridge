251343974f808955856c9a58e832d28726babea2
SjorsJessen <sjors.jessen@gmail.com>
2023-01-15 01:24:46 +0100

    Added reuse gltf (shoe) example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { NgtAmbientLight, NgtSpotLight, NgtPointLight } from "@angular-three/core/lights";
 import { NgtCanvas } from "@angular-three/core";
 import { CubesComponent } from "./cubes/cubes.component";
+import { ReuseGltfComponent } from "./reuse-gltf/reuse-gltf.component";
 import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: 'app.component.html',
-  imports: [NgtCanvas, NgtAmbientLight, NgtSpotLight, NgtPointLight, CubesComponent]
+  imports: [NgtCanvas, NgtAmbientLight, NgtSpotLight, NgtPointLight, CubesComponent, ReuseGltfComponent]
 })
 export class AppComponent {}
