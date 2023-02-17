a73e8bdf0641dededb719b404e4df95e55685784
Sjors Jessen <sjors.jessen@movares.nl>
2023-02-17 12:27:54 +0100

    Cleaned up project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { NgtAmbientLight, NgtSpotLight, NgtPointLight } from "@angular-three/core/lights";
 import { NgtCanvas } from "@angular-three/core";
-import { CubesComponent } from "./cubes/cubes.component";
-import { ReuseGltfComponent } from "./reuse-gltf/reuse-gltf.component";
+import { ReuseGltfComponent } from "./reuse-gltf.component";
 import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: 'app.component.html',
-  imports: [NgtCanvas, NgtAmbientLight, NgtSpotLight, NgtPointLight, CubesComponent, ReuseGltfComponent]
+  template: `<sandbox-reuse-gltf></sandbox-reuse-gltf>`,
+  imports: [NgtCanvas, NgtAmbientLight, NgtSpotLight, NgtPointLight, ReuseGltfComponent]
 })
 export class AppComponent {}
